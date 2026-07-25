09228de0594d7e5fcf4118ea5b72e1080c5b690a
Screded <104632381+Screded@users.noreply.github.com>
2026-07-25 17:46:40 +0200

    Dashboard meteo PWA (ESP32-C3 + Adafruit IO)

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = "meteo-shell-v1";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+  }
+});
